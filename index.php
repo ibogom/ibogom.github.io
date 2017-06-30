e7fed1b00ca30fb996aad66516bdc0d5290b2d57
Ievgen <ievgen.bogomolov@gmail.com>
2017-06-30 15:03:23 +0300

    iStudio project init

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Imagine studio design studio</title>
+<link href="css/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="js/respond.min.js" type="text/javascript"></script>
+<script src="js/jquery.scrollTo-min.js" type="text/javascript"></script>
+<script src="js/animation.js"></script>
+</head>
+<body>
+<div class="navigation">
+    <div class="nav_wrapper">
+        <div class="s_menu">
+            <img src="img/m_menu.png" />
+        </div>
+             <div class="small_menu">
+                <div><a href="#">О Нас</a></div>
+                <div><a href="#services">Цены</a></div>
+                <div><a href="#portfolio">Портфолио</a></div>
+                <div><a href="#">Контакты</a></div>
+             </div>
+        <div class="anchor">
+        <a href="#"></a>
+        </div>
+        <div class="email">
+            <a href="#"><img src="img/mail.png" /></a>
+        </div>
+    </div>
+</div>
+<div class="menu_site_wrapper">
+  <div class="header">
+  	<div class="main">
+    	<div class="menu">
+        	<div><a href="#">О Нас</a></div>
+            <div><a href="#services">Цены</a></div>
+            <div><a href="#portfolio">Портфолио</a></div>
+            <div><a href="#">Контакты</a></div>
+        </div>
+    </div>
+  </div>
+<div class="logo_wrapper">  
+  <div class="main">
+        <div class="logo">
+            <img src="img/logo.png" />
+            <p><span>мир</span> ваших возможностей</p>
+        </div>
+  </div>
+</div>
+<div class='content text' id='about'>
+		<h2>
+        ~
+        <br/>
+        О Нас 
+        </h2>
+        <div>
+             <p>Уже более трех лет мы занимаемся созданием сайтов и работами в различных направлениях рекламной деятельности. Представительства нашей веб-студии успешно работают в Киеве, Харькове и Донецке.
+    Опыт в разработке эффективных рекламных решений дает нам возможность создавать сайты, которые отвечают всем требованиям наших заказчиков, позволяют привлечь и заинтересовать целевую аудиторию и помогают проводить эффективный поиск новых клиентов.
+    </p>
+        </div>
+</div>
+	 <?php include("blocks/services.php"); ?>
+     <?php include("blocks/portfolio.php"); ?>
+  </div>
+</body>
+</html>
